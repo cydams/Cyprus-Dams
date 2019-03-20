9ba99903202f942c6f022d4894d2777061f8f9a1
cydams <mariosmsk@yahooo.com>
2019-03-20 18:34:13 +0200

    update

--- update.bat
+++ update.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\Git\git-cmd.exe" "run.sh"
+cd Cyprus-Dams
+"C:\Program Files\Git\git-cmd.exe" "C:\Users\mkiria01\OneDrive - University of Cyprus\Wordpress - Dams - Data\Cyprus-Dams\run.sh"
