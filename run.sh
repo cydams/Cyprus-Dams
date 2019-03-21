bd0ac0c32aa9b0a9cb4ff284d88b38461ed5d8da
cydams <mariosmsk@yahooo.com>
2019-03-21 13:14:25 +0200

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@ git add .
 git commit -m "update"
 git pull origin master
 git push origin master
+Set WshShell = WScript.CreateObject("WScript.Shell")
+WshShell.Run "C:\Windows\System32\cmd.exe"
+WshShell.AppActivate "C:\Windows\System32\cmd.exe"
+WshShell.SendKeys"spjm -s "IP" -user admin -quiet -t"
+WshShell.SendKeys "{ENTER}"
 :wq
