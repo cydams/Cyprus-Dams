0bb3962c488dc2d3d938a922f8ba1b297c8515d7
cydams <mariosmsk@gmail.com>
2019-09-18 19:20:33 +0300

    update

--- update.bat
+++ update.bat
@@ -1,3 +1,3 @@
-taskkill /im git-cmd.exe /f
+taskkill /im "C:\Program Files\Git\git-cmd.exe" /f
 cd Cyprus-Dams
 "C:\Program Files\Git\git-cmd.exe" "E:\OneDrive - University of Cyprus\Wordpress - Dams - Data\Cyprus-Dams\run.sh"
